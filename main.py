f4bb6eb3d4ee696cf9658c472f1bb24c22cf3204
Hky20062007 <126642200+Hky20062007@users.noreply.github.com>
2023-03-02 06:28:18 +0000

    ghh

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-
+# Kingsley
+# ~~~~ Library App ~~~~
+#
+# Scan book ISBN
+# If the book is loaned out then return it
+# Display Book Details
+# Scan Member ID
+# Display Member details
+# Get the rate
+#
+# Add 7 days to date as Duedate
+# Add Date, Duedate, Book ISBN and Member ID to loan table
+# 
+# Display a message eg.Duedate
+#
+# Returning a book:
+#      Get the date
+#      Find the loan ID where the number ID and the book ISBN is and not returned
+#      Input the returndate into the loan table
